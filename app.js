1c6543fd0f9f262799560ea7a036570cdd44068e
unknown <hafidgameboy@gmail.com>
2026-05-24 00:00:51 +0700

    awal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const db = require('./db.js');
 
 const app = express();
-const port = process.env.PORT || 5775;
+const port = 5775;
 
 app.use(cors());
 app.use(express.json());

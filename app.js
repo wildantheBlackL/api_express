1c8a5daffb4cc7710547dce180a68991e4cfa60e
unknown <hafidgameboy@gmail.com>
2026-06-09 15:34:07 +0700

    fix

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const axios = require('axios');
 const app = express();
 const port = 5775;
 
-app.options('/*(.*)', cors({
+const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 

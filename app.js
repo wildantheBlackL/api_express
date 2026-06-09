9b301054f0a58555b08e53d4641b2c7b613d7b82
unknown <hafidgameboy@gmail.com>
2026-06-09 15:30:20 +0700

    fix cors vercel

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const axios = require('axios');
 const app = express();
 const port = 5775;
 
-app.use(cors({
+app.options('*', cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.get("/status", (req, res) => {
